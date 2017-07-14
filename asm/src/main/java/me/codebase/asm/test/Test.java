d9758d4d8921eccb3f2510a9096b917c297b6f8d
cdx <614141165@qq.com>
2017-07-14 17:05:30 +0800

    first commit;

---
+++ asm/src/main/java/me/codebase/asm/test/Test.java
@@ -0,0 +1,11 @@
+package me.codebase.asm.test;
+
+/**
+ * Created by chendong on 2017/7/14.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ASM!");
+    }
+}
