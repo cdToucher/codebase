390e6f28889cb832a0f9185db90d92ae6d311c50
cdx <614141165@qq.com>
2017-08-29 14:30:54 +0800

    aspect issue;

---
+++ java8/src/main/java/me/codebase/java8/ProxyAspectJCiglib/JdkProxy.java
@@ -0,0 +1,18 @@
+package me.codebase.java8.ProxyAspectJCiglib;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * Created by chendong on 2017/8/29.
+ */
+public class JdkProxy implements InvocationHandler {
+
+
+
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
