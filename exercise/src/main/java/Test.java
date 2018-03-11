6cb8a5c3168443630021a14f2bfe1123f2c21a95
cdx <614141165@qq.com>
2018-03-11 14:28:47 +0800

    learn some info;

--- exercise/src/main/java/Test.java
+++ exercise/src/main/java/Test.java
@@ -1,47 +1,16 @@
-import java.util.Arrays;
-
 /**
  * Created by chendong on 2017/7/24.
  */
 public class Test {
 
 
-    static int[] arr = {1, 4, 2, 5, 9, 0, 6, 3, 7, 8};
-
     public static void main(String[] args) {
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+        Test test = new Test();
+        ClassLoader classLoader = test.getClass().getClassLoader();
 
-    private static void sort(int[] arr) {
-        sort(0, arr.length - 1, arr);
-    }
-
-    private static void sort(int low, int high, int[] arr) {
-        if (low < high) {
-            int foo = sorts(low, high, arr);
-            sort(low, foo - 1, arr);
-            sort(foo + 1, high, arr);
-        }
-    }
-
-    private static int sorts(int low, int high, int[] arr) {
-        int foo = (low + high) / 2;
-        swap(foo, high, arr);
-        int temp = low;
-        for (int i = low; i < high; i++) {
-            if (arr[i] < arr[high]) {
-                swap(temp, i, arr);  //?
-                temp++;
-            }
-        }
-        swap(temp, high, arr);
-        return temp;
-    }
-
-    private static void swap(int i, int j, int[] arr) {
-        int foo = arr[i];
-        arr[i] = arr[j];
-        arr[j] = foo;
+        System.out.println(classLoader);                           // Application ClassLoader  -> Customer ClassLoader  (User application jars/classes)
+        System.out.println(classLoader.getParent());               // Ext ClassLoader          -> ext     or -Djava.ext.dirs jars/classes
+        System.out.println(classLoader.getParent().getParent());   // BootStrap ClassLoader    -> jre/lib or -Xbootclasspath  jars/classes
+        //last class loader is written by 'c';
     }
 }
